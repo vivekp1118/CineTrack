1451a6b5a60dca632f106fc88f75959a5ca94c83
Vivek <vivrkprajapati9986@gmail.com>
2023-02-11 05:39:24 +0530

    Responsive Issue Fix

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
 const btnSearch = document.querySelector(".btn-search")
-const movieContainerEl =  document.querySelector('.container-movie')
+const movieContainerEl = document.querySelector('.container-movie')
 const checkLocalStorage = JSON.parse(localStorage.getItem("localWatchlistArray"))
 let movieArray = []
 let watchlistArray = []
 let totalMoviePages = ""
 
-if(checkLocalStorage)
-{
+if (checkLocalStorage) {
     watchlistArray = checkLocalStorage
 }
-function loadaing(){
-     movieContainerEl.innerHTML=`
+function loadaing() {
+    movieContainerEl.innerHTML = `
             <div class="logo-container">
                       <img src="assert/load.svg" class="loading">
             </div>
           `
 }
 
-function getCurruntPage(){
+function getCurruntPage() {
     return document.body.classList.contains('page-movie') ? "movie" : "watchlist"
 }
-function genratePageNumber(numOfPages){
-    let pageNumHtml = Array(numOfPages).fill(0).map((item,index) => item=index+1)
-    pageNumHtml = pageNumHtml.map(item => item = 
-    `<span id="${item}" data-page=${item} class="number">${item}</span>`).join("")
-                
-    movieContainerEl.innerHTML += 
-                        `<div class="page-number">
+function genratePageNumber(numOfPages) {
+    let pageNumHtml = Array(numOfPages).fill(0).map((item, index) => item = index + 1)
+    pageNumHtml = pageNumHtml.map(item => item =
+        `<span id="${item}" data-page=${item} class="number">${item}</span>`).join("")
+
+    movieContainerEl.innerHTML +=
+        `<div class="page-number">
                             ${pageNumHtml}
                         </div>`
 }
 
 // Movie Page
-if(getCurruntPage()==="movie")
-{ 
-    document.querySelector('.my-watchlist').addEventListener('click',()=>{
-    window.location.href = "watchlist.html"})
+if (getCurruntPage() === "movie") {
+    document.querySelector('.my-watchlist').addEventListener('click', () => {
+        window.location.href = "watchlist.html"
+    })
 
-    btnSearch.addEventListener('click',()=>{
+    btnSearch.addEventListener('click', () => {
         movieArray = []
         const searchText = document.querySelector('.search-txet').value
-        if(searchText=="")
-        {
+        if (searchText == "") {
             setEmptyContainer("emptySearch")
         }
-        else{
-            getMovieId(searchText,1)
+        else {
+            getMovieId(searchText, 1)
         }
     })
 
-    document.addEventListener('click',(e)=>{
-        if(e.target.dataset.imdb){
+    document.addEventListener('click', (e) => {
+        if (e.target.dataset.imdb) {
             const tagetElementId = e.target.id
             document.getElementById(tagetElementId)
             {
@@ -59,50 +57,46 @@ if(getCurruntPage()==="movie")
             }
             updateWatchlistArray(e.target.dataset.imdb)
         }
-        else if(e.target.dataset.page)
-        {
+        else if (e.target.dataset.page) {
             movieArray = []
             const searchText = document.querySelector('.search-txet').value
-            getMovieId(searchText,parseInt(e.target.id))
+            getMovieId(searchText, parseInt(e.target.id))
         }
     })
 
-    function updateWatchlistArray(movieId){
-        const checkArray = watchlistArray.find(item=>item.imdbID===movieId)
-        if(checkArray)
-        {
+    function updateWatchlistArray(movieId) {
+        const checkArray = watchlistArray.find(item => item.imdbID === movieId)
+        if (checkArray) {
             window.alert("Already in Watchlist")
         }
-        else
-        {
-            let watchlistObj = movieArray.filter(item=> item.imdbID  === movieId)[0]
+        else {
+            let watchlistObj = movieArray.filter(item => item.imdbID === movieId)[0]
             watchlistArray.push(watchlistObj)
             updateLocalStorage()
         }
     }
-    function getMovieId(searchText,pageNo){
+    function getMovieId(searchText, pageNo) {
         loadaing()
         fetch(`https://www.omdbapi.com/?apikey=367e161c&s=${searchText}&page=${pageNo}`)
-            .then(res=>res.json())
-            .then(data=>{
-                    totalMoviePages = Math.ceil(data.totalResults /10)
-                    if(data.Response==="True"){
-                        const moviesIdArray = data.Search.map(item=>item.imdbID) 
-                        fetchMovies(moviesIdArray) 
-                    }
-                    else{
-                       setEmptyContainer("movieContainer")
-                    }
+            .then(res => res.json())
+            .then(data => {
+                totalMoviePages = Math.ceil(data.totalResults / 10)
+                if (data.Response === "True") {
+                    const moviesIdArray = data.Search.map(item => item.imdbID)
+                    fetchMovies(moviesIdArray)
+                }
+                else {
+                    setEmptyContainer("movieContainer")
+                }
             })
     }
 
-    async function fetchMovies(data){
-        for (let item of data)
-        {
-            item = item.replace('"','')
+    async function fetchMovies(data) {
+        for (let item of data) {
+            item = item.replace('"', '')
             await fetch(`https://www.omdbapi.com/?apikey=367e161c&i=${item}`)
-                .then(res=>res.json())
-                .then(data=> movieArray.push(data))
+                .then(res => res.json())
+                .then(data => movieArray.push(data))
         }
         render(movieArray)
         genratePageNumber(totalMoviePages)
@@ -110,48 +104,43 @@ if(getCurruntPage()==="movie")
 }
 
 // Watchlist Page
-if(getCurruntPage()==="watchlist")
-{
-    document.querySelector(".container-movie").style.padding ="20px 20px"
-    document.querySelector('.go-to-search').addEventListener('click',()=>{
+if (getCurruntPage() === "watchlist") {
+    document.querySelector(".container-movie").style.padding = "20px 20px"
+    document.querySelector('.go-to-search').addEventListener('click', () => {
         window.location.href = "index.html"
-     })
-     
+    })
+
     render(watchlistArray)
-    if(watchlistArray.length===0)
-    {
+    if (watchlistArray.length === 0) {
         setEmptyContainer("watchlistContainer")
     }
-    
-    document.addEventListener("click",(e)=>{
-         if(e.target.dataset.imdb)
-         {
+
+    document.addEventListener("click", (e) => {
+        if (e.target.dataset.imdb) {
             removeArrayObject(e.target.dataset.imdb)
             render(watchlistArray)
-         }
-         if(watchlistArray.length===0)
-            {
-                setEmptyContainer("watchlistContainer")
-            }
-        
-     })
+        }
+        if (watchlistArray.length === 0) {
+            setEmptyContainer("watchlistContainer")
+        }
+
+    })
 }
 
 // Utils
-function checkExistingElement(item){
-    return checkLocalStorage.filter(oldItem => oldItem.imdbID  === item).length > 0  ? true : false
+function checkExistingElement(item) {
+    return checkLocalStorage.filter(oldItem => oldItem.imdbID === item).length > 0 ? true : false
 }
-function removeArrayObject(itemId)
-{
+function removeArrayObject(itemId) {
     watchlistArray = watchlistArray.filter(item => item.imdbID != itemId)
     updateLocalStorage()
 }
-function updateLocalStorage(){
-    localStorage.setItem("localWatchlistArray",JSON.stringify(watchlistArray))
-} 
-function setEmptyContainer(message){
-    if(message==="watchlistContainer"){
-         movieContainerEl.innerHTML = `
+function updateLocalStorage() {
+    localStorage.setItem("localWatchlistArray", JSON.stringify(watchlistArray))
+}
+function setEmptyContainer(message) {
+    if (message === "watchlistContainer") {
+        movieContainerEl.innerHTML = `
                 <div class="logo-container">
                         <p>Your Watchlist is litte empty </p>     
                         <p class="font-dark"> 
@@ -163,8 +152,8 @@ function setEmptyContainer(message){
                     </div> 
             `
     }
-    else if(message==="movieContainer"){
-         movieContainerEl.innerHTML = `
+    else if (message === "movieContainer") {
+        movieContainerEl.innerHTML = `
                              <div class="logo-container">
                                 <p>
                                     Movie Not Found Please Try another Search
@@ -172,29 +161,29 @@ function setEmptyContainer(message){
                             </div> 
                         `
     }
-    else if(message==="emptySearch"){
-       movieContainerEl.innerHTML=`
+    else if (message === "emptySearch") {
+        movieContainerEl.innerHTML = `
             <div class="logo-container">
                 <p>
                       Try Putting some text into search box and search again
                 </p>
-            </div>` 
+            </div>`
     }
 
 }
 
-function render(data){
-    let html =""
-    let icon = `fa-circle-minus` 
-    
-    data.forEach(item=>{
-        if(checkLocalStorage && getCurruntPage()=="movie"){
-             icon = checkExistingElement(item.imdbID) ? `fa-circle-check`  : `fa-circle-plus`
-        }else{
-             icon = `fa-circle-plus`
+function render(data) {
+    let html = ""
+    let icon = `fa-circle-minus`
+
+    data.forEach(item => {
+        if (checkLocalStorage && getCurruntPage() == "movie") {
+            icon = checkExistingElement(item.imdbID) ? `fa-circle-check` : `fa-circle-plus`
+        } else {
+            icon = `fa-circle-plus`
         }
-        
-        html+=`
+
+        html += `
                 <div class="movie-card">
                     <div class="movie-image" style="background-image:url('${item.Poster}')"></div>
                     <div class="movie-details">
@@ -215,6 +204,6 @@ function render(data){
                 </div> 
         `
     })
-   movieContainerEl.innerHTML = html
+    movieContainerEl.innerHTML = html
 }
 
